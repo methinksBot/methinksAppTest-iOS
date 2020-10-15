5f678f4f565579d15e71f955cb776d9118d63155
Philip Yun <philip@methinks.io>
2020-10-15 01:43:43 -0700

    app test sdk updated

---
+++ methinksAppTest.framework/Headers/methinksAppTest.h
@@ -0,0 +1,19 @@
+//
+//  methinksAppTest.h
+//  methinksAppTest
+//
+//  Created by Philip Yun on 10/13/20.
+//  Copyright © 2020 hansimmmi@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for methinksAppTest.
+FOUNDATION_EXPORT double methinksAppTestVersionNumber;
+
+//! Project version string for methinksAppTest.
+FOUNDATION_EXPORT const unsigned char methinksAppTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <methinksAppTest/PublicHeader.h>
+
+
---
+++ methinksAppTest_LITE.framework/Headers/methinksAppTest_LITE.h
@@ -0,0 +1,22 @@
+//
+//  methinksAppTest_LITE.h
+//  methinksAppTest-LITE
+//
+//  Created by Philip Yun on 10/13/20.
+//  Copyright © 2020 hansimmmi@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#ifndef methinksAppTest_LITE_h
+//#define methinksAppTest_LITE_h
+#import "MTKAppTestSDK.h"
+//#endif
+//! Project version number for methinksAppTest_LITE.
+//FOUNDATION_EXPORT double methinksAppTest_LITEVersionNumber;
+
+//! Project version string for methinksAppTest_LITE.
+//FOUNDATION_EXPORT const unsigned char methinksAppTest_LITEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <methinksAppTest_LITE/PublicHeader.h>
+
+
